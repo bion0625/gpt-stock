1083596027e381c4cfb2f515fd4fda188e1ea2c2
bion0625 <stxtory@gmail.com>
2025-04-30 15:00:58 +0900

    포트폴리오 기본 기능 추가가

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import requests 
 from bs4 import BeautifulSoup
 
+# 실행 코드
+# 기본
+#  .\.venv\Scripts\activate
+# 서버버
+# uvicorn main:app --reload
+
 app = FastAPI()
 
-# MOCK_DATA = {
-#     "005930":{"symbol":"005930", "name":"삼성전자", "price": 75200},
-#     "000660":{"symbol":"000660", "name":"SK하이닉스", "price": 136500}
-# }
+portfolio = {}
+
+class StockItem(BaseModel):
+    symbol: str
+    amount: int
+
+# 종목 추가
+@app.post("/portfolio")
+def add_stock(item: StockItem):
+    if item.symbol in portfolio:
+        portfolio[item.symbol] += item.amount
+    else:
+        portfolio[item.symbol] = item.amount
+    return {"message": "추가 완료", "portfolio": portfolio}
+
+# 실시간 주가 크롤링 함수
+def get_price(symbol: str):
+    url = f"https://finance.naver.com/item/main.nhn?code={symbol}"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, "html.parser")
+    tag = soup.select_one("p.no_today span.blind")
+    return tag.text if tag else "조회 불가"
+
+# 포트폴리오 조회
+@app.get("/portfolio")
+def get_portfolio():
+    result = []
+    for symbol, amount in portfolio.items():
+        price = get_price(symbol)
+        result.append({
+            "symbol": symbol,
+            "amount": amount,
+            "price": price
+        })
+    return result
 
 @app.get("/")
 def read_root():
@@ -15,11 +54,6 @@ def read_root():
 
 @app.get("/stocks/{symbol}")
 def get_stock(symbol: str):
-    # stock = MOCK_DATA.get(symbol)
-    # if stock:
-    #     return stock
-    # else:
-    #     return {"error": "해당 종목이 없습니다."}
     url = f"https://finance.naver.com/item/main.nhn?code={symbol}"
     headers = {"User-Agent": "Mozilla/5.0"} # 봇 차단 우회용
 
