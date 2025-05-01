43966aaf0523a4722132afb22a2b93402a589c16
bion0625 <bion0625@gmail.com>
2025-05-01 20:21:32 +0900

    frontend 기본 설정(UI-css: tailwindcss 설정)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-200">
+      <h1 className="text-4xl text-blue-600 font-bold">
+        Tailwind 강제 테스트 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
